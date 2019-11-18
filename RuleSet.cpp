9a04990578caff2e5c8755b382d2694acce94675
Skorjanc Ivona <ivona.skorjanc@avl.com>
2019-11-18 13:07:11 +0100

    Add commutative rules

--- RuleSet.cpp
+++ RuleSet.cpp
@@ -17,17 +17,30 @@ void RuleSet::applyAllRules(ExpressionTree *expression)
 
     bool reset = true;
     vector<Rule *>::iterator itr;
+    int ruleAppliedBefore = rules.size(); //anti infinite loop mechanism
+    int cnt = 0;
     do
     {
         reset = false;
-        for (int i = 0; i < rules.size(); i++)
+
+        int i = 0;
+        for (i = 0; i < rules.size(); i++)
         {
             if (rules[i]->applyRule(expression))
             {
+                cout << "New expression: ";
+                expression->prefix();
+                cout << "\n";
+
                 reset = true;
                 break;
             }
         }
+        if (ruleAppliedBefore == i && ++cnt >= 4)
+        {
+            reset = false;
+        }
+        ruleAppliedBefore = i;
     } while (reset);
 
     cout << "Final result: ";
