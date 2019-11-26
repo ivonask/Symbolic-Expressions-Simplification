8ea60f27e7517ff714b3e02e1d60b19e532f60a2
ivonask <ivona.skorjanc@fer.hr>
2019-11-26 13:20:54 +0100

    Add all extracted expressions from logs

--- inputLogs/extract1.sh
+++ inputLogs/extract1.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-cd logs
+#first argument: directory of the logs
+cd $1
+if test -f extracted.txt; then
+    rm extracted.txt
+fi
 touch extracted.txt
 for file in *; do
     grep "<Tree" $file | tail -1 | sed 's/[[:blank:]]*<Tree size=\"[[:digit:]]*\">//g' | sed 's/<\/Tree>[[:blank:]]*//g' >> extracted.txt
